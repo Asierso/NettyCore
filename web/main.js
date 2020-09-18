dc2a0c1127f52169e78cf0ee78e5240f404710e1
Asierso <33327643+Asierso@users.noreply.github.com>
2020-09-18 21:35:29 +0200

    Update main.js

--- web/main.js
+++ web/main.js
@@ -4,3 +4,9 @@ function download()
 }
 var version = "1.2.1";
 document.getElementsById("ver1").innerHTML = "a";
+
+function main()
+{
+    window.open("asierso.com/Posts/nettycore.html","_self");
+}
+main();
